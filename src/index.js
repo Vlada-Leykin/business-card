ad47a595e1278e7f31edcef1e993a3ce1ad9adea
VladaLe <vlada.leykin@gmail.com>
2024-04-10 12:22:10 +0200

    add bootstrap infra with webpack

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+// Import our custom CSS
+import './scss/styles.scss';
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap';
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.js';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <App />,
-
-  // </React.StrictMode>
-// <h1>Rick and Morty</h1>
+  <React.StrictMode>
+    <App />
+    ,
+  </React.StrictMode>,
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import autoprefixer from 'autoprefixer';
 
 export default (env, argv) => {
     return {
@@ -11,6 +12,34 @@ export default (env, argv) => {
                   loader: 'babel-loader',
                 },
               },
+              {
+                test: /\.(scss)$/,
+                use: [
+                  {
+                    // Adds CSS to the DOM by injecting a `<style>` tag
+                    loader: 'style-loader'
+                  },
+                  {
+                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                    loader: 'css-loader'
+                  },
+                  {
+                    // Loader for webpack to process CSS with PostCSS
+                    loader: 'postcss-loader',
+                    options: {
+                      postcssOptions: {
+                        plugins: [
+                          autoprefixer
+                        ]
+                      }
+                    }
+                  },
+                  {
+                    // Loads a SASS/SCSS file and compiles it to CSS
+                    loader: 'sass-loader'
+                  }
+                ]
+              }
             ],
           },
           plugins: [
