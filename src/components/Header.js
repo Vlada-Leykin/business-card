5a2092b522e67deea28d396adb281a83fcd2aa92
VladaLe <vlada.leykin@gmail.com>
2024-04-10 13:32:46 +0200

    add profile picture to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ProfilePicture from '../images/profilepic.jpg';
 
 function Header() {
   return (
-    <h1>
-      Vlada Leykin
-    </h1>
+    <header className="container d-flex">
+      <img src={ProfilePicture} alt="Vlada Leykin" width="40" height="40" />
+      <h1>Vlada Leykin</h1>
+    </header>
   );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,67 @@
+import * as path from 'node:path'
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import autoprefixer from 'autoprefixer';
 
+const __dirname = new URL('.', import.meta.url).pathname;
+
 export default (env, argv) => {
-    return {
-        module: {
-            rules: [
-              {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                },
-              },
-              {
-                test: /\.(scss)$/,
-                use: [
-                  {
-                    // Adds CSS to the DOM by injecting a `<style>` tag
-                    loader: 'style-loader'
-                  },
-                  {
-                    // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                    loader: 'css-loader'
-                  },
-                  {
-                    // Loader for webpack to process CSS with PostCSS
-                    loader: 'postcss-loader',
-                    options: {
-                      postcssOptions: {
-                        plugins: [
-                          autoprefixer
-                        ]
-                      }
-                    }
-                  },
-                  {
-                    // Loads a SASS/SCSS file and compiles it to CSS
-                    loader: 'sass-loader'
-                  }
-                ]
-              }
-            ],
+  return {
+    entry: {
+      index: './src/index.js',
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+     clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-          plugins: [
-            new HtmlWebpackPlugin({
-              template: './public/index.html',
-              filename: './index.html',
-            }),
-          ],
-    };
+        },
+        {
+          test: /\.(scss)$/,
+          use: [
+            {
+              // Adds CSS to the DOM by injecting a `<style>` tag
+              loader: 'style-loader'
+            },
+            {
+              // Interprets `@import` and `url()` like `import/require()` and will resolve them
+              loader: 'css-loader'
+            },
+            {
+              // Loader for webpack to process CSS with PostCSS
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    autoprefixer
+                  ]
+                }
+              }
+            },
+            {
+              // Loads a SASS/SCSS file and compiles it to CSS
+              loader: 'sass-loader'
+            }
+          ]
+        }
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+      }),
+    ],
+  };
 }
